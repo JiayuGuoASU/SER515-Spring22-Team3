5cb3043d168c396ae5742169f0f370d261f315ea
zhuoranlzr <zhuoran3@asu.edu>
2022-02-16 14:48:40 -0700

    create sample robot in python package by using nav2

--- src/Anton_description/setup.py
+++ src/Anton_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'Anton_description'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/**')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
