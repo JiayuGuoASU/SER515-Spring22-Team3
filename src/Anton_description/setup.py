de6b5a4bd46099ac74ed59eddb89e95df1fcc2c5
JiayuGuoASU <jguo123@asu.edu>
2022-02-18 20:02:16 -0700

    let the robots move manually
    finished tasks of US2 TASK 25. 26, 27, 28

--- src/Anton_description/setup.py
+++ src/Anton_description/setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/**')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/**')),
+        (os.path.join('share', package_name, 'config'), glob('config/**')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
