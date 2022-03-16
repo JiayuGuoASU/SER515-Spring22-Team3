99d13313e37f0584903acc92cccb350b0a7a8f4d
zhuoranlzr <zhuoran3@asu.edu>
2022-03-16 04:15:22 -0700

    Task 63 64 65

--- src/Anton_description/launch/display_rviz2.launch.py
+++ src/Anton_description/launch/display_rviz2.launch.py
@@ -42,13 +42,13 @@ def generate_launch_description():
         arguments=['-entity', 'Anton', '-topic', 'robot_description'],
         output='screen'
     )
-    robot_localization_node = launch_ros.actions.Node(
-       package='robot_localization',
-       executable='ekf_node',
-       name='ekf_filter_node',
-       output='screen',
-       parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
-)
+   # robot_localization_node = launch_ros.actions.Node(
+     #  package='robot_localization',
+     #  executable='ekf_node',
+     #  name='ekf_filter_node',
+     #  output='screen',
+     #  parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+#)
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
@@ -64,6 +64,6 @@ def generate_launch_description():
         # joint_state_publisher_gui_node,
         robot_state_publisher_node,
         spawn_entity,
-        robot_localization_node,
+        # robot_localization_node,
         rviz_node
     ])
