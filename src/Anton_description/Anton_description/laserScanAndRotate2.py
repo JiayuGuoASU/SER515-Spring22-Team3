0c1f7c68f28da7de2b29a28c23e420964a95ebd4
GuoGuitang <jguo123@asu.edu>
2022-03-16 18:05:27 -0700

    US 8, TASK 76
    try another algorithm to let the robot automatically move without collision

---
+++ src/Anton_description/Anton_description/laserScanAndRotate2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+class WriterNode(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.pub_vel = self.create_publisher(Twist, "/demo/cmd_vel", 10)
+        self.sub_vel = self.create_subscription(LaserScan, "/scan", self.callback , 10);
+        self.msg=Twist();
+        self.msg.linear.x=0.2;
+        self.msg.angular.z=0.0;
+        self.pub_vel.publish(self.msg)
+    def callback(self, msg):
+        print(msg.ranges[0]);
+        msg2=Twist()
+        msg2.linear.x=0.2
+        msg2.angular.z=0.0;
+        minright=min(msg.ranges[0:45])
+        minleft=min(msg.ranges[315:360])
+        if minright<0.5 and minleft<0.5:
+            msg2.linear.x=-0.1
+            msg2.angular.z=0.5
+            self.pub_vel.publish(msg2)
+            return
+        if minleft<0.5:
+            msg2.linear.x=-0.1
+            msg2.angular.z=-0.5      
+        if minright<0.5:
+            msg2.linear.x=-0.1
+            msg2.angular.z=0.5 
+        self.pub_vel.publish(msg2)
+    def __del__(self):
+        msg2=Twist()
+        msg2.linear.x=0.0
+        msg2.angular.z=0.0;
+        self.pub_vel.publish(msg2)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = WriterNode("laserScanAndRotate2")
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
