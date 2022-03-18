331cc6d8ae5222f555c7db521ab359606baec27e
Maaz Ahmed <elfilosof@protonmail.com>
2022-03-18 14:58:56 -0700

    added options for close and better error handling Fixed launch bug with manual control script

--- App.py
+++ App.py
@@ -1,38 +1,17 @@
+from cProfile import label
 from tkinter import *
 from tkinter import ttk
 import subprocess
+import os
+
+from numpy import size
 
 root = Tk()
-root.geometry("400x200")
-frame = Frame(root)
-frame.pack()
 
-leftframe = Frame(root)
-leftframe.pack(side=LEFT)
-
-rightframe = Frame(root)
-rightframe.pack(side=RIGHT)
-
-label = Label(frame, text="Pick a control option and submit")
-label.pack()
-
-label2 = Label(leftframe, text="Manual control")
-label2.pack(padx=10, pady=10)
-
-
-# button1 = Button(rightframe, text="Manual", command=lambda: print("Manual"))
-# button1.pack(padx=3, pady=3)
-
-# label3 = Label(leftframe, text="Automatic control")
-# label3.pack(padx=10, pady=10)
-
-
-# button1 = Button(rightframe, text="Automatic")
-# button1.pack(padx=3, pady=3)
 
 def manual():
-    subprocess.call(['gnome-terminal', '-x', './automatic.sh'])
-    return
+    cmd = ['gnome-terminal', '-x', './manual.sh']
+    proc = subprocess.Popen(cmd)
 
 
 def run():
@@ -43,10 +22,15 @@ def auto():
     subprocess.call(['gnome-terminal', '-x', './automatic.sh'])
 
 
+def close():
+    os.system("kill -9 " + str(PPID))
+
+
 def run1():
     if(Combo.get() == "Manual"):
         print("Manual")
-        manual()
+        run1.ppid = manual()
+        print(run1.ppid)
     elif(Combo.get() == "Automatic"):
         auto()
         print("Automatic")
@@ -99,6 +83,7 @@ vlist = ["Automatic", "Manual"]
 Combo = ttk.Combobox(frame, values=vlist)
 Combo.set("Select")
 Combo.pack(padx=5, pady=5)
+
 button3 = Button(frame, text="Submit", command=run1)
 button3.pack(padx=5, pady=5)
 
