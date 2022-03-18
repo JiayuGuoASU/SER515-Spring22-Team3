c89cf08aa962f5e6dfdc2d64d12181edeb88c722
Maaz Ahmed <elfilosof@protonmail.com>
2022-03-18 14:57:12 -0700

    UI adjustments for clarity

--- App.py
+++ App.py
@@ -54,8 +54,34 @@ def run1():
         print("Run")
         run()
 
-    else:
-        print("Random")
+
+root.geometry("500x500")
+frame = Frame(root)
+frame.pack()
+
+leftframe = Frame(root)
+leftframe.pack(side=LEFT)
+
+rightframe = Frame(root)
+rightframe.pack(side=RIGHT)
+
+labelMain = Label(frame, text="Pick a control option", font=("Helvetica", 20))
+labelMain.pack(padx=10, pady=10)
+
+labelDiv1 = Label(
+    frame, text="_______________________________________", font=("Helvetica", 20))
+
+labelDiv1.pack()
+
+# font size
+labelSub = Label(frame, text="First step: Build the project",
+                 font=("Helvetica", 16))
+labelSub.pack(padx=10, pady=10)
+
+
+buttonSub = Button(frame, text="Build & launch", command=run)
+buttonSub.pack()
+# label3.pack(padx=10, pady=10)
 
 
 vlist = ["Run", "Random", "Automatic", "Manual"]
