e63cfed3662c19c7658a4f770001c52d2f026f48
Maaz Ahmed <elfilosof@protonmail.com>
2022-04-03 17:00:53 -0700

    Updating the UI elements for map customisation (Task #100)

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
-
 from cProfile import label
 from tkinter import *
 from tkinter import ttk
 import subprocess
+from subprocess import call
 import os
 
 from numpy import size
@@ -11,17 +11,16 @@ root = Tk()
 
 
 def manual():
-    cmd = ['gnome-terminal', '-x', './manual.sh']
+    cmd = ["gnome-terminal", "-x", "./manual.sh"]
     proc = subprocess.Popen(cmd)
 
 
-
 def run():
-    subprocess.call(['gnome-terminal', '-x', './fullrun.sh'])
+    subprocess.call(["gnome-terminal", "-x", "./fullrun.sh"])
 
 
 def auto():
-    subprocess.call(['gnome-terminal', '-x', './automatic.sh'])
+    subprocess.call(["gnome-terminal", "-x", "./automatic.sh"])
 
 
 def close():
@@ -29,19 +28,29 @@ def close():
 
 
 def run1():
-    if(Combo.get() == "Manual"):
+    if Combo.get() == "Manual":
         print("Manual")
         run1.ppid = manual()
         print(run1.ppid)
-    elif(Combo.get() == "Automatic"):
+    elif Combo.get() == "Automatic":
         auto()
         print("Automatic")
-    elif(Combo.get() == "Run"):
+    elif Combo.get() == "Run":
         print("Run")
         run()
 
 
-root.geometry("500x500")
+def mapConfig():
+    # run MapWindow.py
+    call(["python3", "Map.py"])
+
+
+def roverConfig():
+    # run MapWindow.py
+    call(["python3", "Rover.py"])
+
+
+root.geometry("500x700")
 frame = Frame(root)
 frame.pack()
 
@@ -54,26 +63,33 @@ rightframe.pack(side=RIGHT)
 labelMain = Label(frame, text="Pick a control option", font=("Helvetica", 20))
 labelMain.pack(padx=10, pady=10)
 
-labelDiv1 = Label(
-    frame, text="_______________________________________", font=("Helvetica", 20))
+labelDiv1 = Label(frame, text="_______________________________________", font=("Helvetica", 20))
 
 labelDiv1.pack()
-
-# font size
-labelSub = Label(frame, text="First step: Build the project",
-                 font=("Helvetica", 16))
+labelSub = Label(frame, text="First step: Configuration", font=("Helvetica", 16))
 labelSub.pack(padx=10, pady=10)
+# font size
+lablel11 = Label(frame, text="Map configuration", font=("Helvetica", 16))
+lablel11.pack(padx=10, pady=10)
 
+bubtton11 = Button(frame, text="Map configuration", command=mapConfig)
+bubtton11.pack()
+
+label12 = Label(frame, text="Rover configuration", font=("Helvetica", 16))
+label12.pack(padx=10, pady=10)
+
+bubtton12 = Button(frame, text="Rover configuration", command=roverConfig)
+bubtton12.pack()
+
+labelDiv2 = Label(frame, text="_______________________________________", font=("Helvetica", 20))
+labelDiv2.pack()
+
+label2 = Label(frame, text="Second step: Build the project", font=("Helvetica", 16))
+label2.pack(padx=10, pady=10)
 
 buttonSub = Button(frame, text="Build & launch", command=run)
 buttonSub.pack()
-# label3.pack(padx=10, pady=10)
 
-
-# button1 = Button(rightframe, text="Automatic")
-# button1.pack(padx=3, pady=3)
-labelDiv2 = Label(
-    frame, text="_______________________________________", font=("Helvetica", 20))
 
 labelDiv3 = Label(frame, text="_______________________________________", font=("Helvetica", 20))
 labelDiv3.pack()
