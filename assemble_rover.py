0cbe2ab901037716bcd7d70979d1c40b487de0a0
Zhuoran Li <zhuoran3@asu.edu>
2022-04-13 22:37:06 -0700

    US-129 task140 re-design the bridge pattern

--- assemble_rover.py
+++ assemble_rover.py
@@ -1,76 +1,78 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Base(metaclass=ABCMeta):
-    def __init__(self, wheel, caster):
+class rover_factory:
+    def __init__(self, base, wheel, caster):
+        self.base = base
         self.wheel = wheel
         self.caster = caster
 
-    @abstractmethod
     def config(self):
+        self.base.config(self)
+        self.wheel.config(self)
+        self.caster.config(self)
+
+
+class Base(metaclass=ABCMeta):
+    @abstractmethod
+    def config(self, rover):
         pass
 
 
 class Base1(Base):
-    def config(self):
+    def config(self, rover):
         print("<xacro:property name=base_width value=0.31/>")
-        self.wheel.config(self)
-        self.caster.config(self)
 
 
 class Base2(Base):
-    def config(self):
+    def config(self, rover):
         print("<xacro:property name=base_width value=10/>")
-        self.wheel.config(self)
-        self.caster.config(self)
 
 
 class Base3(Base):
-    def config(self):
+    def config(self, rover):
         print("<xacro:property name=base_width value=0.20/>")
-        self.wheel.config(self)
-        self.caster.config(self)
 
 
 class Wheel(metaclass=ABCMeta):
     @abstractmethod
-    def config(self, base):
+    def config(self, rover):
         pass
 
 
 class Wheel1(Wheel):
-    def config(self, base):
+    def config(self, rover):
         print("<xacro:property name=wheel_radius value=0.10/>")
 
 
 class Wheel2(Wheel):
-    def config(self, base):
+    def config(self, rover):
         print("<xacro:property name=wheel_radius value=0.10/>")
 
 
 class Wheel3(Wheel):
-    def config(self, base):
+    def config(self, rover):
         print("<xacro:property name=wheel_radius value=0.10/>")
 
 
 class Caster(metaclass=ABCMeta):
     @abstractmethod
-    def config(self, base):
+    def config(self, rover):
         pass
 
 
 class Caster1(Caster):
-    def config(self, base):
+    def config(self, rover):
         print("<xacro:property name=caster_width value=0.15/>")
 
 
 class Caster2(Caster):
-    def config(self, base):
+    def config(self, rover):
         print("<xacro:property name=caster_length value=0.21/>")
 
 
 class Caster3(Caster):
-    def config(self, base):
+    def config(self, rover):
         print("<xacro:property name=caster_xxx value=0.11/>")
 
 
@@ -113,21 +115,19 @@ class WheelFactory(Factory):
 
 
 class BaseFactory(Factory):
-    def __init__(self, component_name, wheel, caster):
+    def __init__(self, component_name):
         self.component_name = component_name
-        self.wheel = wheel
-        self.caster = caster
 
     def create_component(self):
         if self.component_name == "1":
-            base1 = Base1(self.wheel, self.caster)
-            base1.config()
+            base1 = Base1()
+            return base1
         elif self.component_name == "2":
-            base2 = Base2(self.wheel, self.caster)
-            base2.config()
+            base2 = Base2()
+            return base2
         elif self.component_name == "3":
-            base3 = Base3(self.wheel,  self.caster)
-            base3.config()
+            base3 = Base3()
+            return base3
 
 
 def main():
@@ -152,8 +152,11 @@ def main():
     print("3 : base3")
     print("4 : customize")
     base_type = input("option : ")
-    base_factory = BaseFactory(base_type, wheal_object, caster_object)
-    base_factory.create_component()
+    base_factory = BaseFactory(base_type)
+    base_object = base_factory.create_component()
+
+    rover = rover_factory(base_object, wheal_object, caster_object)
+    rover.config()
 
 
 if __name__ == "__main__":
