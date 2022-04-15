92595025e603027e69aa4aa49277def944567bfc
schen253 <schen253@asu.edu>
2022-04-15 22:26:58 +0000

    updated GUI with Map selection

--- App.py
+++ App.py
@@ -4,40 +4,44 @@ from tkinter import ttk
 from tkinter.ttk import Combobox
 import subprocess
 from subprocess import call
-import os
-# import algorithm
+import os, signal
 
 from numpy import size
 
 root = Tk()
 
+processes = []
 
 def manual():
-    # cmd = ["gnome-terminal", "--", "bash", "-c", "cd script; ./manual.sh; read"]
-    subprocess.Popen(["xterm", "-e","./script/manual.sh"])
-    # proc = subprocess.Popen(cmd)
-
+    
+    tmp = subprocess.Popen(["xterm", "-e","./script/manual.sh"])
+    processes.append(tmp)
 
 def run():
-    subprocess.Popen(["xterm", "-e","./script/fullrun.sh"])
-    subprocess.Popen(["xterm", "-e","./script/manual.sh"])
+    tmp = subprocess.Popen(["xterm", "-e","./script/fullrun.sh"])
+    processes.append(tmp)
 
 def auto():
-    subprocess.Popen(["xterm", "-e","./script/automatic.sh"])
-
+    tmp = subprocess.Popen(["xterm", "-e","./script/automatic.sh"])
+    processes.append(tmp)
 
 def getSLAM():
-    subprocess.Popen(["xterm", "-e","./script/downloadmap.sh"])
+    tmp = subprocess.Popen(["xterm", "-e","./script/slam.sh"])
+    processes.append(tmp)
 
 
 def downloadMap():
-    subprocess.Popen(["xterm", "-e","./script/downloadmap.sh"])
+    tmp = subprocess.Popen(["xterm", "-e","./script/downloadmap.sh"])
+    processes.append(tmp)
 
 def close():
-    os.system("kill -9 " + str(PPID))
+    for process in processes:
+        os.kill(process, signal.SIGINT)
+    sys.exit(0)
 
 def mapConfig():
-    call(["python3", "MapChoose.py"])
+    mapName = mapCombo.get() + ".sdf"
+    call(["python3", "MapChoose.py", mapName])
 
 
 def roverConfig():
@@ -47,10 +51,11 @@ def roverConfig():
 def controlAlgorithm():
     algo = algoCombo.get()
     print("you select Algorithm: " + str(algo))
-    call(["python3", "algorithm.py", str(algo)])
+    tmp = subprocess.Popen(["python3", "algorithm.py", str(algo)])
+    processes.append(tmp.pid)
     # subprocess.Popen(["xterm", "-e","python3 algorithm.py"])
 
-root.geometry("500x700")
+root.geometry("500x800")
 frame = Frame(root)
 frame.pack()
 
@@ -60,7 +65,7 @@ leftframe.pack(side=LEFT)
 rightframe = Frame(root)
 rightframe.pack(side=RIGHT)
 
-labelMain = Label(frame, text="Pick a control option", font=("Helvetica", 20))
+labelMain = Label(frame, text="Anton control option", font=("Helvetica", 20))
 labelMain.pack(padx=10, pady=10)
 
 labelDiv1 = Label(frame, text="_______________________________________", font=("Helvetica", 20))
@@ -69,13 +74,17 @@ labelDiv1.pack()
 labelSub = Label(frame, text="First step: Configuration", font=("Helvetica", 16))
 labelSub.pack(padx=10, pady=10)
 
-lablel11 = Label(frame, text="Map configuration:", font=("Helvetica", 16))
+lablel11 = Label(frame, text="Choose a Map and Click Build Map:", font=("Helvetica", 16))
 lablel11.pack(padx=10, pady=10)
 
-# bubtton12 = Button(frame, text="Download map", command=downloadMap)
-# bubtton12.pack()
+mapList = ["RobotCupField", "GasStation", "Museum"]
+mapCombo = Combobox(frame, values=mapList)
+mapCombo.current(0)
 
-bubtton11 = Button(frame, text="Map configuration", command=mapConfig)
+mapCombo.state(["readonly"])
+mapCombo.pack()
+
+bubtton11 = Button(frame, text="Build Map", command=mapConfig)
 bubtton11.pack()
 
 label12 = Label(frame, text="Rover configuration", font=("Helvetica", 16))
@@ -93,6 +102,8 @@ label2.pack(padx=10, pady=10)
 buttonSub = Button(frame, text="Build & launch", command=run)
 buttonSub.pack()
 
+bubtton4 = Button(frame, text="Start Slam", command=getSLAM)
+bubtton4.pack()
 
 labelDiv3 = Label(frame, text="_______________________________________", font=("Helvetica", 20))
 labelDiv3.pack()
@@ -117,11 +128,10 @@ button3.pack(padx=5, pady=5)
 labelDiv4 = Label(frame, text="_______________________________________", font=("Helvetica", 20))
 labelDiv4.pack()
 
-label4 = Label(frame, text="Get the resultant map", font=("Helvetica", 16))
+label4 = Label(frame, text="END", font=("Helvetica", 16))
 label4.pack(padx=10, pady=10)
 
-bubtton4 = Button(frame, text="Result", command=getSLAM)
-bubtton4.pack()
+
 
 button4 = Button(frame, text="close", command=close)
 button4.pack(padx=5, pady=5)
--- MapChoose.py
+++ MapChoose.py
@@ -5,3 +5,4 @@ fileName = sys.argv[1]
 original = r'./src/Anton_description/world/' + str(fileName)
 target = r'./src/Anton_description/world/map.sdf'
 shutil.copyfile(original, target)
+print("Build " + fileName + " Successfully!")
